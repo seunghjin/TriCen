77fada169a26abf776128ddc8bb56b1ca4cb91bf
harutaotaki <ho58@duke.edu>
2025-02-08 17:43:11 -0500

    pass in dummy json file

--- tricen_frontend/views.py
+++ tricen_frontend/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import requests
 
 def dashboard(request):
     print("Request received!")
-    return render(request, 'dashboard/index.html')
+    responses = requests.get('https://dummyjson.com/users').json()
+    return render(request, 'dashboard/index.html', {'responses':responses})
